5eae7b47def5f292c3db6d547d5bfb35f2f94026
Ming Shen <saintshen@gmail.com>
2020-05-30 00:01:26 -0400

    motion move

--- motion/movement.py
+++ motion/movement.py
@@ -4,16 +4,20 @@ import time
 
 BP = brickpi3.BrickPi3()
 
-def Walk(speed=20):
-    BP.set_motor_power(BP.PORT_A + BP.PORT_D, -speed)
-    time.sleep(1)
+def Move(speed, steer=0, m_time=1):
+    
+    BP.set_motor_power(BP.PORT_A, -speed+steer)
+    BP.set_motor_power(BP.PORT_D, -speed-steer)
+    time.sleep(m_time)
     BP.set_motor_power(BP.PORT_A + BP.PORT_D, 0)
 
 def _main():
     parser = argparse.ArgumentParser(description='Movement brickpi3 motor')
     parser.add_argument('--speed', type=int, default=20)
+    parser.add_argument('--steer', type=int, default=0)
+    parser.add_argument('--time', type=int, default=1)
     args = parser.parse_args()
-    Walk(speed=args.speed)
+    Move(speed=args.speed, steer=args.steer, m_time=args.time)
 
 if __name__ == '__main__':
     _main()
