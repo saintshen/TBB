167dcf255dc7a4c6bb5f43b2e3cbe730bc08e0c5
Ming Shen <saintshen@gmail.com>
2021-02-27 21:09:02 -0500

    show chinese

--- junqi/ocr.py
+++ junqi/ocr.py
@@ -1,5 +1,6 @@
 # import the necessary packages
 from imutils.video import VideoStream
+from PIL import ImageFont, ImageDraw, Image
 from imutils import contours
 from imutils import paths
 import numpy as np
@@ -30,6 +31,9 @@ greenUpper = (64, 255, 255)
 vs = VideoStream(src=0).start()
 
 time.sleep(2.0)
+fontpath = "./simsun.ttc"
+font = ImageFont.truetype(fontpath, 32)
+
 
 # keep looping
 while True:
@@ -50,9 +54,9 @@ while True:
     # find contours in the mask and initialize the current
     # (x, y) center of the ball
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
+                            cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    
+
     # only proceed if at least one contour was found
     if len(cnts) > 0:
         for c in cnts:
@@ -61,19 +65,21 @@ while True:
             roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
             roi = cv2.threshold(roi, 20, 255, cv2.THRESH_BINARY_INV)[1]
             roi = imutils.resize(roi, 100)
-            # initialize a list of template matching scores	
+            # initialize a list of template matching scores
             scores = {}
             for (qizi, qiziROI) in qizis.items():
                 result = cv2.matchTemplate(roi, qiziROI,
-                    cv2.TM_CCOEFF)
+                                           cv2.TM_CCOEFF)
                 (_, score, _, _) = cv2.minMaxLoc(result)
                 scores[qizi] = score
-            
+
             qizi = max(scores.items(), key=operator.itemgetter(1))[0]
             cv2.drawContours(frame, [c], -1, (240, 0, 159), 3)
-            cv2.putText(frame, "".join(qizi), (x, y - 15),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
-
+            b, g, r, a = 0, 255, 0, 0
+            img_pil = Image.fromarray(frame)
+            draw = ImageDraw.Draw(img_pil)
+            draw.text((x + 3, y - 20),  qizi, font=font, fill=(b, g, r, a))
+            frame = np.array(img_pil)
 
     # show the frame to our screen
     cv2.imshow("Frame", frame)
